2c991af8e96c862efe50be4d1484186a7aba907c
alexc957 <alexcoronel1995@gmail.com>
2021-07-02 08:25:05 -0500

    get 10 randoms gifs instead of 9

--- src/useGifs.js
+++ src/useGifs.js
@@ -5,7 +5,7 @@ const API_KEY = "<YOUR API KEY GOES HERE>";
 const gf = new GiphyFetch(API_KEY)
 const generateRandonImagesPromises = ()=> {
     const randomGifs = [];
-    for(let i=0;i<9;i++){
+    for(let i=0;i<10;i++){
       randomGifs.push(gf.random({tag: 'cartoons',type: 'gifs' }))
     } 
     return randomGifs;
